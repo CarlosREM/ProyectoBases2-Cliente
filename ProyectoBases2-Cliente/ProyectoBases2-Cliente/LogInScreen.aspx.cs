8d6ef57215e4584397a35200b79547e7ccfcfe3a
fabriciocn16 <40186007+fabriciocn16@users.noreply.github.com>
2019-06-16 15:18:39 -0600

    Added SQL connection to LogIn, update connection string

--- ProyectoBases2-Cliente/ProyectoBases2-Cliente/LogInScreen.aspx.cs
+++ ProyectoBases2-Cliente/ProyectoBases2-Cliente/LogInScreen.aspx.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.SessionState;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
 
 namespace ProyectoBases2_Cliente
 {
@@ -19,11 +23,16 @@ namespace ProyectoBases2_Cliente
 
         protected void btn_login_Click(object sender, EventArgs e)
         {
+            if (!verificarDatosCompletos())
+            {
+                MessageBox.Show("Faltan datos.");
+                return;
+            }
             string user = txtBx_username.Text,
                    password = txtBx_password.Text,
                    userType = cmBx_userType.Text;
 
-            int userResult = temp_LogIn(user, password, userType); //aqui va la funcion de LogIn (esta mas abajo)
+            int userResult = LogIn(user, password, userType); //aqui va la funcion de LogIn (esta mas abajo)
 
             //de aqui en adelante se puede cambiar la parte de los codigos
             //para que correspondan al tipo de usuario que es.
@@ -37,22 +46,22 @@ namespace ProyectoBases2_Cliente
                     Session["admin"] = "0";
             }
 
-                switch (userResult)
+            switch (userResult)
             {
                 case 1:
-                    Response.Redirect("ClientMain.aspx");
+                    Response.Redirect("ClientMain.aspx",false);
                     break;
 
                 case 2:
-                    Response.Redirect("ConsultorMain.aspx");
+                    Response.Redirect("ConsultorMain.aspx", false); //aqui es la pantalla del admin
                     break;
 
                 case 3:
-                    Response.Redirect("FacturadorMain.aspx");
+                    Response.Redirect("FacturadorMain.aspx", false);
                     break;
 
                 case 4:
-                    Response.Redirect("ConsultorMain.aspx");
+                    Response.Redirect("ConsultorMain.aspx", false);
                     break;
 
                 default:
@@ -87,9 +96,90 @@ namespace ProyectoBases2_Cliente
 
         private int LogIn(string user, string password, string userType)
         {
-            //aqui va todo lo de SQL
+            int count = 0;
+            string Constr = WebConfigurationManager.ConnectionStrings["ProyectoBases"].ConnectionString;
+            if (userType.Equals("Cliente"))
+            {
+                string procedureName = "[Empresa].[dbo].[spVerificarCliente]";
+                SqlConnection con = new SqlConnection(Constr);
+                SqlCommand cmd = new SqlCommand(procedureName, con);
+                SqlDataReader reader = null;
 
-            return 0;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@username", user);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.Add(new SqlParameter("@idCliente", SqlDbType.Int));
+                cmd.Parameters["@idCliente"].Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(new SqlParameter("@nombre", SqlDbType.VarChar, 50));
+                cmd.Parameters["@nombre"].Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(new SqlParameter("@apellido", SqlDbType.VarChar, 50));
+                cmd.Parameters["@apellido"].Direction = ParameterDirection.Output;
+
+                SqlParameter returnParam = cmd.Parameters.Add("@return_value", SqlDbType.Int);
+                returnParam.Direction = ParameterDirection.ReturnValue;
+
+                con.Open();
+
+                reader = cmd.ExecuteReader();
+
+                count = int.Parse(cmd.Parameters["@return_value"].Value.ToString());
+                con.Close();
+
+                if(count == 1)
+                {
+                    string nombre = cmd.Parameters["@nombre"].Value.ToString();
+                    string apellido = cmd.Parameters["@apellido"].Value.ToString();
+                    int idCliente = int.Parse(cmd.Parameters["@idCliente"].Value.ToString());
+
+                    Session["nombreCliente"] = nombre;
+                    Session["apellidoCliente"] = apellido;
+                    Session["idCliente"] = idCliente;
+                }
+
+                Debug.WriteLine("Return value: " + count);
+
+            }
+            else if (userType.Equals("Empleado"))
+            {
+                string procedureName = "[Empresa].[dbo].[spVerificarEmpleado]";
+                SqlConnection con = new SqlConnection(Constr);
+                SqlCommand cmd = new SqlCommand(procedureName, con);
+                SqlDataReader reader = null;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@username", user);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.Add(new SqlParameter("@idEmpleado", SqlDbType.Int));
+                cmd.Parameters["@idEmpleado"].Direction = ParameterDirection.Output;
+               
+                SqlParameter returnParam = cmd.Parameters.Add("@return_value", SqlDbType.Int);
+                returnParam.Direction = ParameterDirection.ReturnValue;
+
+                con.Open();
+
+                reader = cmd.ExecuteReader();
+
+                count = int.Parse(cmd.Parameters["@return_value"].Value.ToString());
+                con.Close();
+
+                if(count > 0)
+                {
+                    Session["idEmpleado"] = count;
+                }
+
+                Debug.WriteLine("Return value: " + count);
+            }
+
+            return count;
+        }
+
+        private Boolean verificarDatosCompletos()
+        {
+            if (txtBx_username.Text.Equals("") || txtBx_password.Text.Equals(""))
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
